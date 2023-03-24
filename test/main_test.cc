7449b135c5be7f9824ea434feed2edc8c1a493b2
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-03-24 18:18:26 +0100

    :white_check_mark: test: update tests

--- test/main_test.cc
+++ test/main_test.cc
@@ -2,18 +2,8 @@
 // Created by Stefano on 3/12/2023.
 //
 #include <gtest/gtest.h>
-#include <SDL2/SDL.h>
-#include <camera.h>
-#include <draw.h>
 #include <map.h>
 #include <geometry.h>
-
-const map::tMap kBaseMap = {{1, 1, 1, 1},
-                            {0, 0, 0, 0},
-                            {0, 0, 0, 0},
-                            {0, 0, 0, 0},
-                            {0, 0, 0, 0},
-                            {1, 1, 1, 1}};
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
@@ -22,10 +12,10 @@ int main(int argc, char **argv) {
 
 TEST(findHorizontalWallIntersection_Forward, Expect_FirstHoriontalWallIntersection_Forward) {
 
-  map::Map map(kBaseMap);
+  map::Map map = map::Map::getBasicMap();
 
-  camera::Position expected = {187, 63};
-  auto result = draw::findHorizontalWallIntersection({96, 224}, geometry::k60_degree, map);
+  position::Position expected = {187, 63};
+  auto result = geometry::findHorizontalWallIntersection({96, 224}, geometry::k60_degree, map);
 
   EXPECT_NEAR(expected.x, result.x, 3);
   EXPECT_NEAR(expected.y, result.y, 3);
@@ -33,10 +23,10 @@ TEST(findHorizontalWallIntersection_Forward, Expect_FirstHoriontalWallIntersecti
 
 TEST(findFirstHorizontalIntersection_Forward, Expect_FirstHorizontalIntersection_Forward) {
 
-  map::Map map(kBaseMap);
+  map::Map map = map::Map::getBasicMap();
 
-  camera::Position expected = {115, 191};
-  auto result = draw::findFirstHorizontalIntersection({96, 224}, geometry::k60_degree, tan(geometry::k60_degree));
+  position::Position expected = {115, 191};
+  auto result = geometry::findFirstHorizontalIntersection({96, 224}, geometry::k60_degree, tan(geometry::k60_degree));
 
   EXPECT_NEAR(expected.x, result.x, 1);
   EXPECT_NEAR(expected.y, result.y, 1);
@@ -44,10 +34,10 @@ TEST(findFirstHorizontalIntersection_Forward, Expect_FirstHorizontalIntersection
 
 TEST(findHorizontalWallIntersection_Back, Expect_FirstHoriontalWallIntersection_Back) {
 
-  map::Map map(kBaseMap);
+  map::Map map = map::Map::getBasicMap();
 
-  camera::Position expected = {151, 320};
-  auto result = draw::findHorizontalWallIntersection({96, 224}, geometry::k300_degree, map);
+  position::Position expected = {151, 320};
+  auto result = geometry::findHorizontalWallIntersection({96, 224}, geometry::k300_degree, map);
 
   EXPECT_NEAR(expected.x, result.x, 3);
   EXPECT_NEAR(expected.y, result.y, 3);
@@ -55,10 +45,10 @@ TEST(findHorizontalWallIntersection_Back, Expect_FirstHoriontalWallIntersection_
 
 TEST(findFirstHorizontalIntersection_Back, Expect_FirstHorizontalIntersection_Back) {
 
-  map::Map map(kBaseMap);
+  map::Map map = map::Map::getBasicMap();
 
-  camera::Position expected = {115, 256};
-  auto result = draw::findFirstHorizontalIntersection({96, 224}, geometry::k300_degree, tan(geometry::k300_degree));
+  position::Position expected = {115, 256};
+  auto result = geometry::findFirstHorizontalIntersection({96, 224}, geometry::k300_degree, tan(geometry::k300_degree));
 
   EXPECT_NEAR(expected.x, result.x, 1);
   EXPECT_NEAR(expected.y, result.y, 1);
@@ -66,10 +56,10 @@ TEST(findFirstHorizontalIntersection_Back, Expect_FirstHorizontalIntersection_Ba
 
 TEST(findVerticalWallIntersection_Right, Expect_FirstVerticalWallIntersection_Right) {
 
-  map::Map map(kBaseMap);
+  map::Map map = map::Map::getBasicMap();
 
-  camera::Position expected = {192, 57.92f};
-  auto result = draw::findVerticalWallIntersection({96, 224}, geometry::k60_degree, map);
+  position::Position expected = {192, 57.92f};
+  auto result = geometry::findVerticalWallIntersection({96, 224}, geometry::k60_degree, map);
 
   EXPECT_NEAR(expected.x, result.x, 3);
   EXPECT_NEAR(expected.y, result.y, 3);
@@ -77,10 +67,10 @@ TEST(findVerticalWallIntersection_Right, Expect_FirstVerticalWallIntersection_Ri
 
 TEST(findFirstVerticalIntersection_Right, Expect_FirstVerticalIntersection_Right) {
 
-  map::Map map(kBaseMap);
+  map::Map map = map::Map::getBasicMap();
 
-  camera::Position expected = {128, 168.64};
-  auto result = draw::findFirstVerticalIntersection({96, 224}, geometry::k60_degree, tan(geometry::k60_degree));
+  position::Position expected = {128, 168.64};
+  auto result = geometry::findFirstVerticalIntersection({96, 224}, geometry::k60_degree, tan(geometry::k60_degree));
 
   EXPECT_NEAR(expected.x, result.x, 1);
   EXPECT_NEAR(expected.y, result.y, 1);
@@ -88,10 +78,10 @@ TEST(findFirstVerticalIntersection_Right, Expect_FirstVerticalIntersection_Right
 
 TEST(findVerticalWallIntersection_Left, Expect_FirstVerticalWallIntersection_Left) {
 
-  map::Map map(kBaseMap);
+  map::Map map = map::Map::getBasicMap();
 
-  camera::Position expected = {64, 58};
-  auto result = draw::findVerticalWallIntersection({160, 224}, geometry::k120_degree, map);
+  position::Position expected = {64, 58};
+  auto result = geometry::findVerticalWallIntersection({160, 224}, geometry::k120_degree, map);
 
   EXPECT_NEAR(expected.x, result.x, 3);
   EXPECT_NEAR(expected.y, result.y, 3);
@@ -99,10 +89,10 @@ TEST(findVerticalWallIntersection_Left, Expect_FirstVerticalWallIntersection_Lef
 
 TEST(findFirstVerticalIntersection_Left, Expect_FirstVerticalIntersection_Left) {
 
-  map::Map map(kBaseMap);
+  map::Map map = map::Map::getBasicMap();
 
-  camera::Position expected = {64, 166};
-  auto result = draw::findFirstVerticalIntersection({96, 224}, geometry::k120_degree, tan(geometry::k120_degree));
+  position::Position expected = {64, 166};
+  auto result = geometry::findFirstVerticalIntersection({96, 224}, geometry::k120_degree, tan(geometry::k120_degree));
 
   EXPECT_NEAR(expected.x, result.x, 1);
   EXPECT_NEAR(expected.y, result.y, 1);
